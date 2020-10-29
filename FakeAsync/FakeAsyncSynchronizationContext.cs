8e534fe51fcd5d8d97765563255ebb77c5129b9c
Anatoly Buranov <2414704+Tolyandre@users.noreply.github.com>
2020-10-29 23:07:58 +0400

    Support locks progress

---
+++ FakeAsync/FakeAsyncSynchronizationContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FakeAsyncs
+{
+    public class FakeAsyncSynchronizationContext : SynchronizationContext
+    {
+        private readonly Queue<(SendOrPostCallback Callback, object State)> _queue = new Queue<(SendOrPostCallback Callback, object State)>();
+        private object _lockObj = new object();
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _queue.Enqueue((d, state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Wait(IntPtr[] waitHandles, bool waitAll, int millisecondsTimeout)
+        {
+            return base.Wait(waitHandles, waitAll, millisecondsTimeout);
+        }
+
+        public override SynchronizationContext CreateCopy()
+        {
+            return this;
+        }
+
+        private void RunNextCallback()
+        {
+            var lockTaken = false;
+            Monitor.TryEnter(_lockObj, ref lockTaken);
+
+            if (!lockTaken)
+                throw new FakeAsyncConcurrencyException("Only one thread is allowed to run inside FakeAsync. " + FakeAsyncConcurrencyException.DefaultTaskSchedulerWarning);
+
+            try
+            {
+                var (callback, state) = _queue.Dequeue();
+                callback(state);
+            }
+            finally
+            {
+                Monitor.Exit(_lockObj);
+            }
+        }
+
+        /// <summary>
+        /// Fake for synchronous waits.
+        /// </summary>
+        public void FakeSyncWait()
+        {
+            var ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+        }
+
+    }
+}
--- Tests/FakeAsyncTests/PollyTests.cs
+++ Tests/FakeAsyncTests/PollyTests.cs
@@ -11,7 +11,7 @@ namespace FakeAsyncTests
         private readonly FakeAsync _fakeAsync = new FakeAsync();
 
         [Fact]
-        public void WaitAndRetry()
+        public void WaitAndRetryAsync()
         {
             var policy = Policy
               .Handle<ApplicationException>()
@@ -35,6 +35,41 @@ namespace FakeAsyncTests
             var ex = Assert.Throws<ApplicationException>(() => _fakeAsync.Isolate(() =>
             {
                 var testing = policy.ExecuteAsync(() => Method());
+
+                _fakeAsync.Tick(TimeSpan.FromSeconds(100));
+                Assert.Equal(4, count);
+
+                return testing;
+            }));
+
+            Assert.Equal(4.ToString(), ex.Message);
+        }
+
+        [Fact]
+        public void WaitAndRetry()
+        {
+            var policy = Policy
+              .Handle<ApplicationException>()
+              .WaitAndRetry(new[]
+              {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(3)
+              });
+
+            int count = 0;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+            async Task Method()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+            {
+                count++;
+                throw new ApplicationException(count.ToString());
+            }
+
+            var ex = Assert.Throws<ApplicationException>(() => _fakeAsync.Isolate(() =>
+            {
+                var testing = policy.Execute(() => Method());
 
                 _fakeAsync.Tick(TimeSpan.FromSeconds(100));
                 Assert.Equal(4, count);
